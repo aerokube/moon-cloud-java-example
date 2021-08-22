0e0ae1bff9c1dd754b8b6f8029ee99445b25c84e
Ivan Krutov <vania-pooh@aerokube.com>
2021-08-22 16:20:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Moon Cloud Java Example
+
+This example demonstrates how to use [Moon Cloud](https://aerokube.com/moon-cloud/) from your [Java](https://java.com/) code.
+
+```
+$ mvn clean test
+```
---
+++ src/test/java/com/aerokube/selenoid/MoonCloudExampleTest.java
@@ -0,0 +1,51 @@
+package com.aerokube.selenoid;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+class MoonCloudExampleTest {
+
+    private RemoteWebDriver driver;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        ChromeOptions capabilities = new ChromeOptions();
+        capabilities.setCapability("browserVersion", "92.0");
+//        capabilities.setCapability("moon:options", Map.of(
+//                "enableVideo", true,
+//                "enableVNC", true,
+//                "name", "MyCoolTest",
+//                "screenResolution", "1024x768",
+//                "sessionTimeout", "3m"
+//        ));
+        driver = new RemoteWebDriver(new URL("https://username:password@my-company.cloud.aerokube.com/wd/hub"), capabilities);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+    @Test
+    void testTakeScreenshot() throws IOException {
+        driver.get("https://aerokube.com/");
+        Path screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE).toPath();
+        Files.copy(screenshot, Paths.get(driver.getSessionId() + ".png"));
+    }
+}
